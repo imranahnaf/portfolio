d3429894c1e60e88bf2da2666c4234a0b702b379
imranahnaf <mdemranhossen147@gmail.com>
2021-12-19 01:19:10 +0600

    portfolio psd

---
+++ js/costom.js
@@ -0,0 +1,27 @@
+$(function () {
+    // =====***** service slick js starts *****===== //
+    $(".service-slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 500,
+        dots: false,
+        infinite: true,
+        prevArrow: '<i class="fa fa-angle-left prevarrow" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-angle-right nextarrow" aria-hidden="true"></i>',
+        
+
+        // responsive starts
+        responsive: [{
+            breakpoint: 1199,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+
+        },]
+        // responsive ends
+    });
+    // =====***** service slick js ends *****===== //
+});
