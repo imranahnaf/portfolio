c4e8e61d8caae12c6416aa9299a3f4e13de5c8d7
imranahnaf <mdemranhossen147@gmail.com>
2021-12-20 07:19:57 +0600

    portfolio testimonials section complete

--- js/costom.js
+++ js/costom.js
@@ -8,10 +8,9 @@ $(function () {
         speed: 500,
         dots: false,
         infinite: true,
-        prevArrow: '<i class="fa fa-angle-left prevarrow" aria-hidden="true"></i>',
-        nextArrow: '<i class="fa fa-angle-right nextarrow" aria-hidden="true"></i>',
+        prevArrow: '<i class="fa fa-long-arrow-left prevarrow" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-long-arrow-right nextarrow" aria-hidden="true"></i>',
         
-
         // responsive starts
         responsive: [{
             breakpoint: 1199,
@@ -23,4 +22,29 @@ $(function () {
         // responsive ends
     });
     // =====***** service slick js ends *****===== //
+
+
+    // =====***** testimonials slick js starts *****===== //
+    $(".testimonials-slider").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 500,
+        dots: false,
+        infinite: true,
+        prevArrow: '<i class="fa fa-long-arrow-left prevarrow" aria-hidden="true"></i>',
+        nextArrow: '<i class="fa fa-long-arrow-right nextarrow" aria-hidden="true"></i>',
+        
+        // responsive starts
+        responsive: [{
+            breakpoint: 1199,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+        },]
+        // responsive ends
+    });
+    // =====***** restimonials slick js ends *****===== //
 });
